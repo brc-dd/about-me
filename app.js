0e1601ee2c28b0597d8fe62133b565a32297ee9a
Divyansh Singh <40380293+brc-dd@users.noreply.github.com>
2020-02-25 14:44:21 +0530

    switch to https

--- app.js
+++ app.js
@@ -15,6 +15,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(function (request, response) {
+  if (!request.secure) {
+    response.redirect("https://" + request.headers.host + request.url);
+  }
+});
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html')))
 app.get('/home', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html')))
